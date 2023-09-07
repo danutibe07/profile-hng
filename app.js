7e714940e9047d3297d550069c3d0d1f8c52034e
danutibe07 <danielutibe07@gmail.com>
2023-09-07 15:36:27 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const Time = document.getElementById("currentTime");
+const Day = document.getElementById("currentDay");
+  
+const currentDate = new Date();
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const currentDay = daysOfWeek[currentDate.getDay()];
+const currentTimeInMilliseconds = currentDate.getTime();
+
+// const currentHour = currentDate.getHours().toString().padStart(2, '0');
+// const currentMinute = currentDate.getMinutes().toString().padStart(2, '0');
+// const currentSecond = currentDate.getSeconds().toString().padStart(2, '0');
+// const currentTime = `${currentHour}:${currentMinute}:${currentSecond}`;
+
+Day.innerHTML = `<strong>Current Day:</strong> ${currentDay}`;
+Time.innerHTML = `<strong>Current Time (in milliseconds):</strong> ${currentTimeInMilliseconds}`;
